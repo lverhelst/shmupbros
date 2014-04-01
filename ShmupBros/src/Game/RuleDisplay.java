dfacdf6a2211f197272c6c8808072380eef0a6d9
Emery Berg <emery.berg@gmail.com>
2014-04-01 00:50:31 -0700

    adjustable graphs

--- ShmupBros/src/Game/RuleDisplay.java
+++ ShmupBros/src/Game/RuleDisplay.java
@@ -18,9 +18,7 @@ public class RuleDisplay extends javax.swing.JPanel {
     private double x_scale;
     private double y_scale;
     
-    private boolean moving;
-    private double x_move;
-    private double y_move;
+    private int index = -1;
     
     /**
      * Creates new form RuleDisplay
@@ -30,21 +28,49 @@ public class RuleDisplay extends javax.swing.JPanel {
         setRule(Settings.rules.get(0));
         
         this.addMouseListener(new MouseListener(){
+            private double x_move;
+            private double y_move;
 
             @Override
             public void mouseClicked(MouseEvent e) {
                 x_move = e.getX()/x_scale;
                 y_move = (y_max - e.getY()/y_scale)*2;
+                
+                for(int i = 0; i < x_coords.length; ++i) {
+                    if(x_coords[i] + 5 > x_move && x_coords[i] - 5 < x_move && 
+                            y_coords[i] + 0.2 > y_move && y_coords[i] - 0.2 < y_move) {
+                        index = i;
+                        break;
+                    }                            
+                }
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-                moving = true;
+                x_move = e.getX()/x_scale;
+                y_move = (y_max - e.getY()/y_scale)*2;
+                
+                for(int i = 0; i < x_coords.length; ++i) {
+                    if(x_coords[i] + 5 > x_move && x_coords[i] - 5 < x_move && 
+                            y_coords[i] + 0.2 > y_move && y_coords[i] - 0.2 < y_move) {
+                        index = i;
+                        break;
+                    }                            
+                }
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                moving = false;
+                x_move = e.getX()/x_scale;
+                y_move = (y_max - e.getY()/y_scale)*2;
+                
+                if(index != 0 && index != x_coords.length - 1)
+                    x_coords[index] = Math.min(Math.max(x_move, 0), x_total);
+                
+                y_coords[index] = Math.min(Math.max(y_move, 0), y_max);
+                
+                RuleDisplay.this.revalidate();
+                RuleDisplay.this.repaint();
             }
 
             @Override
@@ -53,10 +79,6 @@ public class RuleDisplay extends javax.swing.JPanel {
             @Override
             public void mouseExited(MouseEvent e) {}
         });
-    }
-    
-    public boolean findPosition(int x, int y) {
-        return false;
     }
     
     /**
@@ -119,13 +141,22 @@ public class RuleDisplay extends javax.swing.JPanel {
         
         int last_x = (int)(x_coords[0] * x_scale) + 4;
         int last_y = (int)(y_max - y_coords[0] * y_scale/2) + getHeight()/2;
+        
+        if(index == 0) 
+            g2.setColor(new Color(0f,0f,0.75f));
+        
         g2.fillOval(last_x-4, last_y-4, 8, 8);
         
         for(int i = 1; i < x_coords.length; ++i) {
             int x = (int)(x_coords[i] * x_scale) + 4;
             int y = (int)(y_max - y_coords[i] * y_scale/2) + getHeight()/2;
             
+            g2.setColor(new Color(0.75f,0f,0f));
             g2.drawLine(x, y, last_x, last_y);
+            
+            if(index == i) 
+                g2.setColor(new Color(0f,0f,0.75f));
+            
             g2.fillOval(x-4, y-4, 8, 8);
             
             last_x = x;
